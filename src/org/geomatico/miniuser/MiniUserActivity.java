d5618b307a5ed3398f318127a6a0df6932bedc3f
fergonco <fernando.gonzalez@geomati.co>
2011-10-24 08:45:21 +0200

    Zoom controls

--- src/org/geomatico/miniuser/MiniUserActivity.java
+++ src/org/geomatico/miniuser/MiniUserActivity.java
@@ -5,39 +5,122 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.RelativeLayout;
+import android.widget.ZoomControls;
 import es.prodevelop.gvsig.mini.app.Initializer;
 import es.prodevelop.gvsig.mini.common.CompatManager;
 import es.prodevelop.gvsig.mini.common.IContext;
+import es.prodevelop.gvsig.mini.context.ItemContext;
+import es.prodevelop.gvsig.mini.control.DoubleTapZoom;
+import es.prodevelop.gvsig.mini.control.PanControl;
 import es.prodevelop.gvsig.mini.exceptions.BaseException;
 import es.prodevelop.gvsig.mini.map.MapView;
+import es.prodevelop.gvsig.mini.map.MapViewListener;
 import es.prodevelop.tilecache.renderer.MapRenderer;
 import es.prodevelop.tilecache.renderer.OSMMercatorRenderer;
 
-public class MiniUserActivity extends Activity {
+public class MiniUserActivity extends Activity implements MapViewListener {
+
+	private MapView mapView;
+	private ZoomControls zoomControls;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		DisplayMetrics metrics = new DisplayMetrics();
-		getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
+		mapView = createMapView();
+
+		if (mapView != null) {
+			RelativeLayout relativeLayout = new RelativeLayout(this);
+			relativeLayout.addView(mapView, new RelativeLayout.LayoutParams(
+					LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+
+			zoomControls = new ZoomControls(this);
+			final RelativeLayout.LayoutParams zzParams = new RelativeLayout.LayoutParams(
+					RelativeLayout.LayoutParams.WRAP_CONTENT,
+					RelativeLayout.LayoutParams.WRAP_CONTENT);
+			zzParams.addRule(RelativeLayout.ALIGN_BOTTOM);
+			zzParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+			relativeLayout.addView(zoomControls, zzParams);
+			zoomControls.setId(107);
+			zoomControls.setVisibility(View.VISIBLE);
+
+			zoomControls.setOnZoomInClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					mapView.getMapViewController().zoomIn();
+				}
+			});
+			zoomControls.setOnZoomOutClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					mapView.getMapViewController().zoomOut();
+				}
+			});
+			setContentView(relativeLayout);
+		}
+	}
+
+	private MapView createMapView() {
+		MapView view = null;
 		try {
+			// Initialize
 			try {
-				Initializer.getInstance().initialize(
-						getApplicationContext());
+				Initializer.getInstance().initialize(getApplicationContext());
 			} catch (Exception e) {
 				throw new BaseException(e);
 			}
+
+			// Instantiate MapView
 			IContext context = CompatManager.getInstance()
 					.getRegisteredContext();
 			Handler mapHandler = new Handler();
 			MapRenderer renderer = OSMMercatorRenderer.getMapnikRenderer();
-			MapView view = new MapView(this, mapHandler, context, renderer,
+			DisplayMetrics metrics = new DisplayMetrics();
+			getWindowManager().getDefaultDisplay().getMetrics(metrics);
+			view = new MapView(this, mapHandler, context, renderer,
 					metrics.widthPixels, metrics.heightPixels);
-			setContentView(view);
+
+			// Configure controls
+			view.addControl(new PanControl());
+			view.addControl(new DoubleTapZoom());
+
+			view.addMapViewListener(this);
 		} catch (BaseException e) {
 			Log.e("MiniUser", "Cannot instantiate Mini activity", e);
 			// TODO Tell the user something is really wrong here
 		}
+		return view;
+	}
+
+	@Override
+	public void zoomLevelChanged() {
+		try {
+			MapRenderer r = mapView.getMRendererInfo();
+
+			if (r.getZoomLevel() > r.getZoomMinLevel())
+				zoomControls.setIsZoomOutEnabled(true);
+			else
+				zoomControls.setIsZoomOutEnabled(false);
+
+			if (r.getZOOM_MAXLEVEL() > r.getZoomLevel()) {
+				zoomControls.setIsZoomInEnabled(true);
+			} else {
+				zoomControls.setIsZoomInEnabled(false);
+			}
+		} catch (Exception e) {
+			Log.e("MiniUser", "updateZoomControl: ", e);
+		}
+	}
+
+	@Override
+	public void overlayContextToShow(ItemContext itemContext) {
+	}
+
+	@Override
+	public void defaultContextToShow() {
 	}
 }
